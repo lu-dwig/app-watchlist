8b486c30cf1caaa9a0e6a410af78eb17b5565034
ludwig <murimiludwig@gmail.com>
2022-04-28 12:23:40 +0300

    this will add some files

---
+++ app/views.py
@@ -0,0 +1,15 @@
+# from email import message
+from flask import render_template
+from app import app
+
+# views 
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    
+    '''
+    View movie page function that returns the movie details and its data  
+    '''
+    
+    return render_template('movie.html', id = movie_id)
+
+
